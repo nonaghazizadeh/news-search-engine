279aedb4e3c839b56e7d7ea4530d7ac627a8ba79
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-07-18 13:02:26 +0430

    feat: add base of pages

--- front/search_engine/src/App.vue
+++ front/search_engine/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <side-bar />
     <router-view/>
+    <side-bar />
   </div>
 </template>
+
+<script>
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  
+  name: 'Home',
+  components: {
+    SideBar
+  }
+}
+</script>
 
 <style>
 #app {
