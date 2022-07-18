e1620e4f366603c7a4464941b72f792f3752be1e
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-07-18 12:44:30 +0430

    feat: add side bar component

--- front/search_engine/src/main.js
+++ front/search_engine/src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon";
+
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.component('vue-sidebar-menu-akahon', VueSidebarMenuAkahon);
 
 new Vue({
   router,
