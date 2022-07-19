0ed14b6f656e566f6423fbef5262a283a9576e81
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-07-19 16:59:20 +0430

    feat: add elastic searcher

---
+++ src/elastic_search.py
@@ -0,0 +1,64 @@
+from elasticsearch import Elasticsearch
+import tqdm
+import json
+from src.enums.enums import StaticNum,Path
+from src.query_expansion import *
+import config
+
+
+class ElasticSearch:
+    def __init__(self, query, need_indexing=False, should_expand_query=True):
+        self.client = Elasticsearch(
+            cloud_id=config.CLOUD_ID,
+            basic_auth=("elastic", config.ELASTIC_PASSWORD)
+        )
+        self.body = {}
+        self.query = query
+        self.related_titles = list()
+        self.qe = QueryExpansion(self.query.split())
+        self.qe()
+        self.should_expand_query = should_expand_query
+        self.need_indexing = need_indexing
+
+    def __call__(self):
+        self.create_body_match_query()
+        self.get_result()
+        self.elastic_merge_results()
+
+    def index_data(self):
+        with open(Path.DATA_PATH.value, "r", encoding="utf-8") as text_file:
+            news_dict = json.loads(text_file.read())
+
+        for i in tqdm.tqdm(range(len(news_dict))):
+            temp_doc = news_dict[str(i)]
+            self.client.index(index="contents", id=i + 1, document=temp_doc)
+
+    def create_body_match_query(self, num=StaticNum.DOC_RELATED_NUM.value):
+        self.body = {
+            "from": 0,
+            "size": num,
+            "query": {
+                "match": {
+                    "content": self.query
+                }
+            }
+        }
+
+    def get_result(self, is_qe=False):
+        res = self.client.search(index="contents", body=self.body)['hits']['hits']
+        titles = list()
+        for idx, i in enumerate(res):
+            titles.append(i['_source']['title'])
+        if is_qe:
+            return titles
+        else:
+            self.related_titles = titles
+
+    def elastic_merge_results(self, num=StaticNum.DOC_RELATED_NUM.value):
+        self.query = self.qe.expand_query(0.7)
+        self.create_body_match_query()
+        qe_results = self.get_result(True)
+        res = [*self.related_titles[:num], *qe_results[:num]]
+        res = list(dict.fromkeys(res))
+        for idx, title in enumerate(res):
+            print(f"{idx + 1}\t{title}")
