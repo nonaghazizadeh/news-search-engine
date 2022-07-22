bbb646085f6e1a3849ddd76db4b0754177867e6f
ShayanEmzed <shayan80sama@gmail.com>
2022-07-22 11:06:02 +0430

    feat: add link to crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,7 +3,7 @@ import os
 
 import requests
 from bs4 import BeautifulSoup
-import lxml
+import   lxml
 
 DIR_NAME = 'news'
 os.makedirs(DIR_NAME, exist_ok=True)
@@ -11,11 +11,11 @@ os.makedirs(DIR_NAME, exist_ok=True)
 BASE_URL = "https://www.hamshahrionline.ir"
 POSTFIX_URL = "/page/archive.xhtml?wide=0&ms=0&"
 SUBJECTS_DICT = {6: 'سیاسی', 11: 'جهان', 10: 'اقتصاد', 5: 'جامعه', 7: 'شهر', 21: 'زندگی', 580: 'محله',
-               718: 'فناوری اطلاعات', 20: 'دانش', 26: 'فرهنگ و هنر', 9: 'ورزش'}
+                 718: 'فناوری اطلاعات', 20: 'دانش', 26: 'فرهنگ و هنر', 9: 'ورزش'}
 
 data = {}
 
-TOTAL_PAGES = 50
+TOTAL_PAGES = 12
 idx = 0
 for k, v in SUBJECTS_DICT.items():
     for page_num in range(1, TOTAL_PAGES + 1):
@@ -46,11 +46,10 @@ for k, v in SUBJECTS_DICT.items():
             for text in texts:
                 content += text.text
             if content != "":
-                data[idx] = {'title': title, 'subject': v, 'content': content}
+                data[idx] = {'title': title, 'subject': v, 'link': per_url, 'content': content}
                 idx += 1
 
         print(f"crawling page {page_num} of subject {v} done.")
 
-with open(f"{DIR_NAME}/news.json", "w", encoding="utf-8") as file:
+with open(f"{DIR_NAME}/news_12.json", "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False)
-
